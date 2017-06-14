c94e4ade66ba4b848388768f2a735ab06733b161
Diljeet <jeetds1994@gmail.com>
2017-06-14 15:39:13 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT books.title, books.year FROM books INNER JOIN series ON series.id = books.series_id WHERE books.series_id = 1 ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT name, motto FROM characters ORDER BY length(motto) DESC LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
+'SELECT authors.name, subgenres.name FROM series INNER JOIN authors ON series.author_id = authors.id INNER JOIN subgenres ON subgenres.id = series.subgenre_id GROUP BY authors.name;'
 end
 
 def select_series_title_with_most_human_characters
+  'SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id GROUP BY characters.species, series.title HAVING characters.species = "human" ORDER BY COUNT(characters.species) DESC LIMIT 1;'
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, COUNT(book_id) FROM books JOIN character_books ON books.id = character_books.book_id JOIN characters ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(book_id) DESC"
 end
